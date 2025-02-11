eb803139b21b2686f86b1f6ed3b7b82fb2e519f0
Mirko <moonsethra@gmail.com>
2025-02-11 11:15:01 -0500

    Preview - legalizador 11-02-2025

--- navegacion/legalizador.py
+++ navegacion/legalizador.py
@@ -1,6 +1,6 @@
 from navegacion import sub_menu as sm, ventana_informacion
-from recursos import  label, botones, colors
-from funcionalidad import  web_controller, poliedro, excel
+from recursos import label, botones, colors
+from funcionalidad import web_controller, poliedro, excel
 from subprocess import Popen
 import threading
 import tkinter as tk
@@ -10,50 +10,49 @@ import requests
 
 class Legalizador:
 
-    def __init__(self,master, on_of, alertas):
+    def __init__(self, master, on_of, alertas):
         self.alertas = alertas
         self.etapa = 0
         self.on_of = on_of
         self.poliedro = poliedro.Poliedro(legalizador=True)
         self.excel = excel.Excel_controller()
-        self.link= 'https://poliedrodist.comcel.com.co/'
-        self.link2='https://poliedrodist.comcel.com.co/activaciones/http/REINGENIERIA/pagDispatcherEntradaModernizacion.asp?Site=1'
-        self.titulo = label.Label().create_label(master, 'LEGALIZADOR', 0.2, 0.0, 0.5,0.2, letterSize= 25)
-        self.ventana_informacion =  ventana_informacion.Ventana_informacion(master)
-        self.menu = sm.Sub_menu(master,3, boton1=['ABRIR LISTA', self.abrir_excel], boton2=['ABRIR PAGINA', self.abrir_pagina], boton3=['START', self.ejecuccionHilo])
+        self.link = 'https://poliedrodist.comcel.com.co/'
+        self.link2 = 'https://poliedrodist.comcel.com.co/activaciones/http/REINGENIERIA/pagDispatcherEntradaModernizacion.asp?Site=1'
+        self.titulo = label.Label().create_label(master, 'LEGALIZADOR', 0.2, 0.0, 0.5, 0.2, letterSize=25)
+        self.ventana_informacion = ventana_informacion.Ventana_informacion(master)
+        self.menu = sm.Sub_menu(master, 3, boton1=['ABRIR LISTA', self.abrir_excel], boton2=['ABRIR PAGINA', self.abrir_pagina], boton3=['START', self.ejecuccionHilo])
         self.legalizador = ''
         self.time = tk.StringVar()
         self.time2 = 3
         self.time.set('0')
-        self.titulo = label.Label().create_label(self.menu.submenu, 'Intervalos', 0.0, 0.65, 0.5,0.2, letterSize= 16)
-        self.titulo = label.Label().create_label(self.menu.submenu, 'Ciclos', 0.0, 0.78, 0.5,0.05, letterSize= 16)
-        self.titulo2 = label.Label().create_label(self.menu.submenu, 'Correo', 0.25, 0.83, 0.5,0.05, letterSize= 16)
+        self.titulo = label.Label().create_label(self.menu.submenu, 'Intervalos', 0.0, 0.65, 0.5, 0.2, letterSize=16)
+        self.titulo = label.Label().create_label(self.menu.submenu, 'Ciclos', 0.0, 0.78, 0.5, 0.05, letterSize=16)
+        self.titulo2 = label.Label().create_label(self.menu.submenu, 'Correo', 0.25, 0.83, 0.5, 0.05, letterSize=16)
         input_widget = ctk.CTkEntry(self.menu.submenu, textvariable=self.time)
         input_widget.place(relx=0.5, rely=0.73, relheight=0.05, relwidth=0.2)
         boton = botones.Buttons()
         color = colors.Colors()
         self.okBotton = boton.create_button(self.menu.submenu, 'OK', 0.7, 0.73, 0.15, 0.05, self.cambioIntervalo)
-        self.okBotton.configure(fg_color= color.team, text_color= 'white')
+        self.okBotton.configure(fg_color=color.team, text_color='white')
         self.correo = 'acruz@teamcomunicaciones.com'
         self.correoEdit = tk.StringVar()
         self.correoEdit.set(self.correo)
         self.cookie_header = {}
         self.repeticiones = '1'
         self.repeticionesEdit = tk.StringVar()
-        self.repeticionesEdit.set(self.repeticiones) 
+        self.repeticionesEdit.set(self.repeticiones)
         input_widget2 = ctk.CTkEntry(self.menu.submenu, textvariable=self.correoEdit)
         input_widget2.place(relx=0.15, rely=0.89, relheight=0.05, relwidth=0.7)
         input_widget3 = ctk.CTkEntry(self.menu.submenu, textvariable=self.repeticionesEdit)
         input_widget3.place(relx=0.5, rely=0.79, relheight=0.05, relwidth=0.2)
         self.okBotton2 = boton.create_button(self.menu.submenu, 'confirmar', 0.3, 0.95, 0.40, 0.05, self.cambioCorreo)
-        self.okBotton2.configure(fg_color= color.team, text_color= 'white')
+        self.okBotton2.configure(fg_color=color.team, text_color='white')
         self.okBotton3 = boton.create_button(self.menu.submenu, 'OK', 0.7, 0.79, 0.15, 0.05, self.cambioCiclos)
-        self.okBotton3.configure(fg_color= color.team, text_color= 'white')
+        self.okBotton3.configure(fg_color=color.team, text_color='white')
         
-    
     def abrir_excel(self):
         self.ventana_informacion.write('excel legalizador abierto recuerde cerrar antes de iniciar')
-        p = Popen("src\legalizador\openExcel.bat")
+        p = Popen("src\\legalizador\\openExcel.bat")
         stdout, stderr = p.communicate()
     
     def cambioCiclos(self):
@@ -70,7 +69,7 @@ class Legalizador:
     
     def abrir_pagina(self):
         self.ventana_informacion.write('Navegador abierto')
-        class Abrir_pagina1(web_controller.Web_Controller):pass
+        class Abrir_pagina1(web_controller.Web_Controller): pass
         self.legalizador = Abrir_pagina1(int(self.time.get()))
         self.legalizador.openEdge()
         self.legalizador.selectPage(self.link)
@@ -84,7 +83,7 @@ class Legalizador:
         for i in range(int(self.repeticiones)):
             self.ciclo = True
             self.contador = 0
-            self.excel.leer_excel('src\legalizador\legalizador.xlsx','iccid')
+            self.excel.leer_excel('src\\legalizador\\legalizador.xlsx', 'iccid')
             self.excel.quitarFormatoCientifico('iccid')
             self.excel.quitarFormatoCientifico('imei')
             self.ventana_informacion.write(f'Ciclo {i+1}')
@@ -94,7 +93,7 @@ class Legalizador:
                 else:
                     try:
                         self.min = str(self.excel.excel['min'][self.contador])
-                        self.mensaje= str(self.excel.excel['Mensaje'][self.contador])
+                        self.mensaje = str(self.excel.excel['Mensaje'][self.contador])
                         if str(self.mensaje) != 'nan' and str(self.mensaje) != 'error':
                             self.ventana_informacion.write(f'Legalizacion {self.min} ya realizada o con error ya detectado')
                             self.contador += 1
@@ -107,8 +106,6 @@ class Legalizador:
             self.ventana_informacion.write(f'Ciclo {i+1} finalizado')
         self.on_of(True)
     
-
-    
     def verificar_urls(self):
         self.ventana_informacion.write(f'legalizando numero {self.contador+1} de {self.excel.cantidad}')
         iccid = str(self.excel.excel['iccid'][self.contador])[-12:] 
@@ -119,29 +116,58 @@ class Legalizador:
         self.apellido = str(self.excel.excel['apellido'][self.contador])
         cedula = str(self.excel.excel['cedula'][self.contador]).replace('.0','')
         self.tipoDoc = str(self.excel.excel['tipodoc'][self.contador])
-        urls = [
-            "https://traffic-md-webapp-prd01.traffic.claro.com.co/CaptureData/ValidarConfigProductPrefijos?productId=362&_=1738762903923",
-            f"https://traffic-md-webapp-prd01.traffic.claro.com.co/CaptureData/ValidatIccidErrors?imei={imei}&iccid={iccid}&_=1738762903926",
-            f"https://traffic-md-webapp-prd01.traffic.claro.com.co/CaptureData/GetValPin?phone={min}&docNumber={cedula}&product=362&_=1738762903927"
-        ]
         
         headers = {
             'Cookie': self.cookie_header['Cookie']
         }
         
-        for url in urls:
-            response = requests.get(url, headers=headers)
-            if response.status_code != 200:
-                self.excel.guardar(self.contador, 'Mensaje', f"Error en la URL: {url.replace('https://traffic-md-webapp-prd01.traffic.claro.com.co/CaptureData/', '')}")
-                raise Exception(f"Error en la URL: {url}")
+        # Validar la primera URL
+        url1 = f"https://traffic-md-webapp-prd01.traffic.claro.com.co/CaptureData/ValidatIccidErrors?imei={imei}&iccid={iccid}&_=1738762903926"
+        response1 = requests.get(url1, headers=headers)
+        if response1.status_code == 200:
+            json_response1 = response1.json()
+            if json_response1.get("InfoShowEsim") == False and json_response1.get("EsimErrorMsj") is None:
+                self.ventana_informacion.write("Validación de EsimErrors exitosa")
+            else:
+                self.excel.guardar(self.contador, 'Mensaje', "Error en la validación de EsimErrors")
+                raise Exception("Error en la validación de EsimErrors")
+        else:
+            self.excel.guardar(self.contador, 'Mensaje', f"Error en la URL: {url1}")
+            raise Exception(f"Error en la URL: {url1}")
+
+        # Validar la segunda URL
+        url2 = f"https://traffic-md-webapp-prd01.traffic.claro.com.co/CaptureData/GetValPin?phone={min}&docNumber={cedula}&product=362&_=1738762903927"
+        response2 = requests.get(url2, headers=headers)
+        if response2.status_code == 200:
+            json_response2 = response2.json()
+            if json_response2.get("code") == 0 and json_response2.get("description") is None and json_response2.get("response") is None and json_response2.get("Attempts") is None and json_response2.get("url") is None:
+                self.ventana_informacion.write("Validación de GetValPin exitosa")
+            else:
+                self.excel.guardar(self.contador, 'Mensaje', "Error en la validación de GetValPin")
+                raise Exception("Error en la validación de GetValPin")
+        else:
+            self.excel.guardar(self.contador, 'Mensaje', f"Error en la URL: {url2}")
+            raise Exception(f"Error en la URL: {url2}")
 
         # Hacer la petición GET a la URL final
-        response = requests.get("https://traffic-md-webapp-prd01.traffic.claro.com.co/Validation", headers=headers)
-        if response.status_code != 200:
+        final_url = "https://traffic-md-webapp-prd01.traffic.claro.com.co/Validation"
+        final_response = requests.get(final_url, headers=headers)
+        if final_response.status_code == 200:
+            # Buscar el contenido específico en la respuesta
+            content = final_response.text
+            start_index = content.find("<!-- LeaP Alert A -->")
+            end_index = content.find("<!-- LeaP Alert B -->", start_index)
+            if start_index != -1 and end_index != -1:
+                specific_content = content[start_index:end_index]
+                self.ventana_informacion.write(f"Contenido específico encontrado: {specific_content}")
+            else:
+                self.excel.guardar(self.contador, 'Mensaje', "Error: Contenido específico no encontrado")
+                raise Exception("Error: Contenido específico no encontrado")
+        else:
             self.excel.guardar(self.contador, 'Mensaje', "Error en la URL de validación final")
             raise Exception("Error en la URL de validación final")
-
-        # Hacer la petición POST si todas las peticiones GET fueron exitosas
+        
+        # Si ambas validaciones son exitosas, hacer la petición POST
         post_url = "https://traffic-md-webapp-prd01.traffic.claro.com.co/CaptureData/Index2"
         post_data = {
             "ProductShortcutName": "362 - (GLKC) - Legalizacion Kit Contado",
@@ -180,13 +206,69 @@ class Legalizador:
         post_response = requests.post(post_url, headers=headers, data=post_data)
         if post_response.status_code == 200:
             self.ventana_informacion.write(f"{iccid} procesada correctamente")
+            
+            # Si la petición POST es exitosa, hacer la petición POST adicional
+            demographic_url = "https://traffic-md-webapp-prd01.traffic.claro.com.co/Demographic/Index1"
+            demographic_data = {
+                "PersonalInfo.GreetingId": "O",
+                "PersonalInfo.Name": "HNOS GOMEZ INVERSIONES S.A.S.",
+                "PersonalInfo.LastName": "",
+                "PersonalInfo.Email": "manuel.arango@teamcomunicaciones.com",
+                "PersonalInfo.Phone.PhoneId": "",
+                "PersonalInfo.Phone.PhoneClass": "2",
+                "PersonalInfo.Phone.Prefix": "604",
+                "PersonalInfo.Phone.PhoneNumber": "3131234",
+                "PersonalInfo.EmailInitial": "",
+                "PersonalInfo.DocumentTypeId": "2",
+                "PersonalInfo.Document": "900721484",
+                "PersonalInfo.Address.AddressId": "",
+                "PersonalInfo.Address.AddressClassId": "Otras",
+                "PersonalInfo.Address.Address": "central",
+                "PersonalInfo.Address.Department": "ANTIOQUIA",
+                "PersonalInfo.Address.City": "MEDELLIN",
+                "PersonalInfo.Address.Town": "Central"
+            }
+            demographic_response = requests.post(demographic_url, headers=headers, data=demographic_data)
+            if demographic_response.status_code == 200:
+                demographic_json = demographic_response.json()
+                if demographic_json.get("rta") == True and not demographic_json.get("errores") and demographic_json.get("url") == "/ProductService":
+                    self.ventana_informacion.write("Petición POST a Demographic/Index1 exitosa")
+                    
+                    # Si la petición POST a Demographic/Index1 es exitosa, hacer la petición POST a ProductService/Index1
+                    product_service_url = "https://traffic-md-webapp-prd01.traffic.claro.com.co/ProductService/Index1"
+                    product_service_data = {
+                        "EquipmentPlanDataViewModel.SaleDate": "11/02/2025",
+                        "EquipmentPlanDataViewModel.IMEI": "353167580611861",
+                        "EquipmentPlanDataViewModel.ICCID": "89571017023099423508",
+                        "EquipmentPlanDataViewModel.MobileEquipment": "TCL 4041 T311A GRIS",
+                        "EquipmentPlanDataViewModel.Plan": "Plan Kit Prepago GSM",
+                        "EquipmentPlanDataViewModel.InternalCode": "",
+                        "ValorBussinesPlan2": "0",
+                        "EquipmentPlanDataViewModel.ContractType": "2",
+                        "EquipmentPlanDataViewModel_CfmToFirstInvoice": "false",
+                        "IsCesionDifPostPost": "false",
+                        "EquipmentPlanDataViewModel.InvoiceCustomer": ""
+                    }
+                    product_service_response = requests.post(product_service_url, headers=headers, data=product_service_data)
+                    if product_service_response.status_code == 200:
+                        product_service_json = product_service_response.json()
+                        if product_service_json.get("rta") == True and not product_service_json.get("errores") and product_service_json.get("url") == "/ProductService":
+                            self.ventana_informacion.write("Petición POST a ProductService/Index1 exitosa")
+                        else:
+                            self.excel.guardar(self.contador, 'Mensaje', "Error en la validación de ProductService/Index1")
+                            raise Exception("Error en la validación de ProductService/Index1")
+                    else:
+                        self.excel.guardar(self.contador, 'Mensaje', "Error en la URL de ProductService/Index1")
+                        raise Exception("Error en la URL de ProductService/Index1")
+                else:
+                    self.excel.guardar(self.contador, 'Mensaje', "Error en la validación de Demographic/Index1")
+                    raise Exception("Error en la validación de Demographic/Index1")
+            else:
+                self.excel.guardar(self.contador, 'Mensaje', "Error en la URL de Demographic/Index1")
+                raise Exception("Error en la URL de Demographic/Index1")
         else:
             self.excel.guardar(self.contador, 'Mensaje', "Error en la URL para legalizar")
             raise Exception("Error en la URL de validación final")
-
-
-
-
 
 
         
